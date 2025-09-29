1e545a8248f3e3f7967ab53d20a4d123a386c2ac
raquel <valentinachance25@gmail.com>
2025-09-29 10:15:08 -0300

    array global and new function

--- aula rafaella/script.js
+++ aula rafaella/script.js
@@ -1,32 +1,35 @@
- function adicionarTarefa() {
-   //entrada de input id mencionada en la variable input
-   let inputTarefa = document.getElementById("inputTarefa") 
-   //paragrafo com variavel mensagem
-   const mensagem = "Tarefa adicionada com sucesso!";
-   //crea variable tarea y la convierte en un valor, tim para eliminar espacios en blanco
-   let tarefa = inputTarefa.value.trim();
+let tarefas = []; // array global para armazenar tarefas
 
-   if (tarefa !== "") {
-      console.log(tarefa);
-      document.getElementById("mensagem").textContent = mensagem;
+function adicionarTarefa() {
+   const inputTarefa = document.getElementById("inputTarefa");
+   let mensagem = ["Tarefa adicionada com sucesso!", "Por favor, digite uma tarefa.!"]
+   let tarefaTexto = inputTarefa.value.trim();
+
+   if (tarefaTexto !== "") {
+      tarefas.push(tarefaTexto); // add tarefas ao array
+      console.log(tarefaTexto);
+      document.getElementById("mensagem").textContent = mensagem[0];
       document.getElementById("mensagem").style.color = "green";
-      inputTarefa.value = "";
       inputTarefa.focus();
-      //limpia el mensaje despues de 3 segundos
+      //limpa el mensaje despues de 3 segundos
       setTimeout(() => {
          document.getElementById("mensagem").textContent = "";
       },3000);
-      //agrega la tarea a la lista
-      let listaTarefas = document.getElementById("listaTarefas");
-      //crea el elemento li denomidado novaTarefa
-      let novaTarefa = document.createElement("li");
-      //agrega el texto de la tarea al elemento li
-      novaTarefa.textContent = tarefa;
-      //agrega el elemento li a la lista de tareas
-      listaTarefas.appendChild(novaTarefa); 
+      renderizarTarefas(); // renderiza a lista atualizada
    } else {
-      document.getElementById("mensagem").textContent = "Por favor, digite uma tarefa.";
+      document.getElementById("mensagem").textContent = mensagem[1];
       document.getElementById("mensagem").style.color = "red";
+   }
+   inputTarefa.value = "";
+}
 
+function renderizarTarefas() {
+   const listaTarefas = document.getElementById("listaTarefas");
+   listaTarefas.innerHTML = ""; // limpa a lista antes de renderizar
+
+   for (let i = 0; i < tarefas.length; i++) {
+      let novaTarefa = document.createElement("li");
+      novaTarefa.textContent = tarefas[i];
+      listaTarefas.appendChild(novaTarefa);
    }
 }
