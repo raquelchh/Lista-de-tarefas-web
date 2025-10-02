a826cfc0aa569995fb05c5828b9f2236cfeaeb44
raquel <valentinachance25@gmail.com>
2025-10-01 23:32:27 -0300

    add function message

--- to-do-list/script.js
+++ to-do-list/script.js
@@ -2,42 +2,32 @@ let tarefas = []; // array global para armazenar tarefas
 
 function adicionarTarefa() {
    const inputTarefa = document.getElementById("inputTarefa");
-   let mensagem = "Tarefa adicionada com sucesso!";
    let tarefaTexto = inputTarefa.value.trim();
 
    if (tarefaTexto !== "") {
-      tarefas.push(tarefaTexto); // add tarefas ao array
+      tarefas.push(tarefaTexto); 
       console.log(tarefaTexto);
-      document.getElementById("mensagem").textContent = mensagem
-      document.getElementById("mensagem").style.color = "green";
+      mostrarMensagem("Tarefa adicionada com sucesso!", "green");
       inputTarefa.focus();
-      //limpa el mensaje despues de 3 segundos
-      setTimeout(() => {
-        document.getElementById("mensagem").textContent = "";
-      }, 3000);
-      renderizarTarefas(); // renderiza a lista atualizada
-
+      renderizarTarefas(); 
    } else {
-      document.getElementById("mensagem").textContent = "Por favor, insira uma tarefa válida.";
-      document.getElementById("mensagem").style.color = "red";
+      mostrarMensagem("Por favor, insira uma tarefa válida.", "red");
    }
    inputTarefa.value = "";
 }
 
 function renderizarTarefas() {
    const listaTarefas = document.getElementById("listaTarefas");
-   listaTarefas.innerHTML = ""; // limpa a lista antes de renderizar
+   listaTarefas.innerHTML = ""; 
 
    for (let i = 0; i < tarefas.length; i++) {
       let novaTarefa = document.createElement("li");
       novaTarefa.textContent = tarefas[i];
        
-   
       let buttonRemove = document.createElement("button");
       buttonRemove.className = "remover";
       buttonRemove.textContent = "Remover";
       buttonRemove.onclick = () => removerTarefa(i);
- 
 
       let editTarefa = document.createElement("button");
       editTarefa.className = "editar";
@@ -47,15 +37,13 @@ function renderizarTarefas() {
       novaTarefa.appendChild(buttonRemove);
       novaTarefa.appendChild(editTarefa);
       listaTarefas.appendChild(novaTarefa);
-
    }
-
 }
 
 function removerTarefa(i){
     tarefas.splice(i, 1);
     renderizarTarefas();
-     
+    mostrarMensagem("Tarefa removida com sucesso!", "green");
 }
 
 function editarTarefa(i){
@@ -63,21 +51,24 @@ function editarTarefa(i){
    if (tarefaEditada !== null && tarefaEditada.trim() !== ""){
       tarefas[i] = tarefaEditada.trim();
       renderizarTarefas();
+      mostrarMensagem("Tarefa editada com sucesso!", "green");
+   } else {
+      mostrarMensagem("Edição inválida, tente novamente.", "red");
    }
 }
 
 function limparLista(){
    tarefas.length = 0;
    renderizarTarefas();
-   let mensagemElement = document.getElementById("mensagem");
-   let mensagem = "Lista de tarefas limpa com sucesso";
-   mensagemElement.textContent = mensagem;
-   mensagemElement.style.color = "green";
-   if(tarefas.length == 0 ){
-      setTimeout(() => {
-         mensagemElement.textContent = "";
-      }, 3000);
-   }
-
+   mostrarMensagem("Lista limpa com sucesso!", "green");
 }
+
  
+function mostrarMensagem(mensagem, cor){
+   const mensagemElement = document.getElementById("mensagem");
+   mensagemElement.textContent = mensagem;
+   mensagemElement.style.color = cor;
+   setTimeout(() => {
+      mensagemElement.textContent = "";
+   }, 3000);
+}
