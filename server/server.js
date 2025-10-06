8ffffcd97cd525c8f016c0505b8b86317eb244c0
raquel <valentinachance25@gmail.com>
2025-10-06 17:53:57 -0300

    Adicionando pasta server ao projeto principal

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-const express = required ("express") //usa framework Express 
-const app = express ();              //cria servidor
-app.used(express.json());            //permite receber dados JSON
+const express = require('express'); //usa framework Express 
+const app = express();              //cria servidor
+app.use(express.json());            //permite receber dados JSON
 
 app.get ('/', (req, res) => {
-    res.send("Servidor funcionando"); //rota de test
+    res.send('Servidor funcionando'); //rota de test
 });
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
 });
